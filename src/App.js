9456ea5bc4d94253ac78b6629f24295b45100148
Semilogo Bamidele <emmanbamidele471@gmail.com>
2023-08-11 15:12:57 +0100

    feat: project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 h-full">
+      This is a styled component using Tailwind CSS.
+    </div>
+  );
+}
+
+export default App;
